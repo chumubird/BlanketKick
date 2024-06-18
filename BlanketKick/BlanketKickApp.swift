cafe288522b228368e011e006d976874a2aca7f2
judas <churoo0418@gmail.com>
2024-06-18 11:20:30 +0900

    Initial Commit

---
+++ BlanketKick/BlanketKickApp.swift
@@ -0,0 +1,20 @@
+//
+//  BlanketKickApp.swift
+//  BlanketKick
+//
+//  Created by chul on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BlanketKickApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
