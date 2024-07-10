868e0eae89aedfbd140ef59369f95ec573a95096
judas <churoo0418@gmail.com>
2024-07-10 14:37:55 +0900

    [User] user View basic UI

--- BlanketKick/Main/User/User_View.swift
+++ BlanketKick/Main/User/User_View.swift
@@ -2,9 +2,61 @@
 import SwiftUI
 
 struct User_View: View {
+    
+    @State var userPhoto : Image?
+    @State var userName : String = "user name"
+    
     var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("Here for User profile data etc")
+        
+        
+        ZStack{
+           
+            VStack{
+                
+                VStack{
+                    HStack{
+                        Text("My Page")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .padding()
+                        Spacer()
+                        
+                    }
+                        
+                }
+                Spacer()
+                    .frame(height: 100)
+                
+                
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .foregroundStyle(Gradient(colors: [.blue,.purple]))
+                    .onAppear(perform: {
+                    
+                    })
+                
+                RoundedRectangle(cornerRadius: 25.0)
+                    .stroke(.blue)
+                    .background(.clear)
+                    .frame(width: 230,height: 60)
+                    .overlay {
+                        GradientText(text: userName, gradient: LinearGradient(colors: [.blue,.purple], startPoint: .leading, endPoint: .trailing), fontSize: .headline)
+                            
+                    }
+                    .padding()
+                
+                Spacer()
+                
+
+                
+               
+
+                
+            }
+        }
     }
 }
 
