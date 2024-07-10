0d91bdf53c79a6e4a4b82ae21a2deeade70f6aba
judas <churoo0418@gmail.com>
2024-07-10 11:16:18 +0900

    [File/Add]  Main folder - > User folder /  after login , app will show user page

---
+++ BlanketKick/Main/User/User_View.swift
@@ -0,0 +1,12 @@
+
+import SwiftUI
+
+struct User_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    User_View()
+}
