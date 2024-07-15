1e89ca41094bfb46de17708a55887a1ac6128587
judas <churoo0418@gmail.com>
2024-07-16 05:20:42 +0900

    [UI]  about User_View  <----refactoring---> User_ViewModel

--- BlanketKick/Main/User/User_View.swift
+++ BlanketKick/Main/User/User_View.swift
@@ -3,76 +3,53 @@ import SwiftUI
 
 struct User_View: View {
     
-    @State var userPhoto : Image?
     
-    @State var userEmail : String = "user Email"
-    @State var userName : String = "user name"
+    let viewModel = User_ViewModel()
     
     
     var body: some View {
         
-        
         ZStack{
-           
+            
             VStack{
                 
                 VStack{
+                    
                     HStack{
-                        Text("My Page")
-                            .font(.title)
-                            .fontWeight(.black)
-                            .padding()
+                        
+                        viewModel.TextForTitle()
                         Spacer()
-                        
                     }
-                        
                 }
+                
                 Spacer()
                     .frame(height: 100)
                 
                 
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .foregroundStyle(Gradient(colors: [.blue,.purple]))
-                    .onAppear(perform: {
-                    
-                    })
                 
-                RoundedRectangle(cornerRadius: 25.0)
-                    .stroke(.blue)
-                    .background(.clear)
-                    .frame(width: 230,height: 60)
-                    .overlay {
-                        GradientText(text: userEmail, gradient: LinearGradient(colors: [.blue,.purple], startPoint: .leading, endPoint: .trailing), fontSize: .headline)
-                            
-                    }
-                    .padding(.top)
+                //             user profile DATA
+                //
+                //                user Photo
+                //
+                //                user Email
+                //
+                //                user Name
+                //
                 
                 
-                RoundedRectangle(cornerRadius: 25.0)
-                    .stroke(.blue)
-                    .background(.clear)
-                    .frame(width: 230,height: 60)
-                    .overlay {
-                        GradientText(text: userName, gradient: LinearGradient(colors: [.blue,.purple], startPoint: .leading, endPoint: .trailing), fontSize: .headline)
-                            
-                    }
-                    .padding(.bottom)
+                viewModel.userProfilePhotoImage()
+                
+                viewModel.textForUserData(someView: GradientText(text: viewModel.userEmail, gradient: LinearGradient(colors: [.blue, .purple], startPoint: .leading, endPoint: .trailing), fontSize: .headline), padding: .init(top: 20, leading: 0, bottom: 0, trailing: 0))
+                
+                
+                viewModel.textForUserData(someView: GradientText(text: viewModel.userName, gradient: LinearGradient(colors: [.blue,.purple], startPoint: .leading, endPoint: .trailing), fontSize: .headline), padding: .init(top: 0, leading: 0, bottom: 20, trailing: 0))
                 
                 Spacer()
-                
-
-                
-               
-
-                
             }
         }
     }
 }
+
 
 #Preview {
     User_View()
