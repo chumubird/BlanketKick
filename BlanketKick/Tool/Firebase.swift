e26d57af9035c1301ccbb737929c30ea7264ff9e
judas <churoo0418@gmail.com>
2024-07-20 08:08:51 +0900

    [Test/Done] after CodeRefactoring , everything works well like before

--- BlanketKick/Tool/Firebase.swift
+++ BlanketKick/Tool/Firebase.swift
@@ -27,17 +27,9 @@ class Firebase: ObservableObject {
         return Future {promise in
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let error = error {
-                    
-                    
                     promise(.failure(error))
-                    
-                    
                 } else if let authResult = authResult {
-                    
-                    
                     promise(.success(authResult))
-                    
-                    
                 }
             }
         }
@@ -223,13 +215,6 @@ class Firebase: ObservableObject {
             }
         }
     }
-    
-    
-    
-    
-    
-    
-    
 }
 
 
