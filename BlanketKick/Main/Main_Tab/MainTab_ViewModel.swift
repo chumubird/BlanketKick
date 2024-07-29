cf4bd771ea0f33e5d79e376fd97720d7fb5002a7
judas <churoo0418@gmail.com>
2024-07-29 09:13:22 +0900

    [File]  MainTab_ViewModel file // for  getting user image data and user name data on tab bar item for user

---
+++ BlanketKick/Main/Main_Tab/MainTab_ViewModel.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+import Foundation
+
+import Combine
+
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+
+
+
+
+class MainTab_ViewModel: ObservedObject{
+ 
+    
+    
+    
+    
+}
+
+#Preview {
+    MainTab_View()
+}
