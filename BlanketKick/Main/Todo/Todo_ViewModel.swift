2055836bf1b03470ce7f1ce08b8febf7f9c4dcb1
judas <churoo0418@gmail.com>
2024-07-31 15:13:54 +0900

    [File/add&delete] Todo_View&ViewModel add for item of tab bar // Map folder Deleted

---
+++ BlanketKick/Main/Todo/Todo_ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  Todo_ViewModel.swift
+//  BlanketKick
+//
+//  Created by chul on 7/31/24.
+//
+
+import SwiftUI
+
+struct Todo_ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Todo_ViewModel()
+}
