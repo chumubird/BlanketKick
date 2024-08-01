398e6fe59edfeaec065facfe9372bc47010f320d
judas <churoo0418@gmail.com>
2024-08-01 18:32:18 +0900

    [UI] Todo_view will be designed like metamong todo list

--- BlanketKick/Main/Todo/Todo_View.swift
+++ BlanketKick/Main/Todo/Todo_View.swift
@@ -4,8 +4,22 @@
 import SwiftUI
 
 struct Todo_View: View {
+    
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+    
+    @State var textFieldValue : String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        VStack{
+            
+            
+            Spacer()
+            GradientStrokeTextField(gradient: LinearGradient(colors: [.pink,.purple], startPoint: .topLeading, endPoint: .bottomTrailing), placeholderValue: "ToDo Value", bindingValue: $textFieldValue)
+            
+        }
     }
 }
 
